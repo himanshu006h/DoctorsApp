dacb88d5647d6e80b525e82cf2936129bf796738
Himanshu Saraswat <himanshu.saraswat@yatra.com>
2019-12-06 10:30:10 +0530

    Initial Commit

---
+++ DoctorsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoctorsApp
+//
+//  Created by Himanshu Saraswat on 06/12/19.
+//  Copyright © 2019 Himanshu Saraswat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
